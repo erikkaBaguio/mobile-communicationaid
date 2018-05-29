ea05a631fd6384a0c73d3318fbfebe31bd032be2
guray27 <gurayraphael27@gmail.com>
2018-05-29 16:48:28 +0800

    added forum template and profile templates

--- www/w3_bootstrap/js/index.js
+++ www/w3_bootstrap/js/index.js
@@ -11,31 +11,65 @@
 // change;
 function get_info(){
     var acc_id = localStorage.getItem("acc_id");
-    alert(acc_id+"heloooooooo")
-    var url  = "https://api-pic-a-talk.herokuapp.com/api/user/"+acc_id;
+    var acc_type = localStorage.getItem("acc_type");
+    var url  = "https://cryptic-fjord-60133.herokuapp.com/api/parent/"+acc_id;
     var xhr  = new XMLHttpRequest()
     xhr.open('GET', url, true)
     xhr.onreadystatechange = function () { 
         if (xhr.readyState == 4 && xhr.status == 200) {
             var json = JSON.parse(xhr.responseText);
-            alert(json.user)
-            console.log(json);
-            localStorage.setItem('acc_name', json.user.username);
-            localStorage.setItem('acc_email', json.user.email);
-            localStorage.setItem('acc_type', json.user.acc_type);
-            var acc_type = localStorage.getItem("acc_type");
-            if (acc_type = 1){
-                location = "mode.html"
-            }
-            if (acc_type = 2) {
-                location = "t_mode.html"
-            }  
 
-            console.table(json);
-            alert("success")
-        } else {
-            console.error(json);
+            if (json.message == "no user found" && acc_type == 1){
+                location = "pform.html"
+            }
+            
+            else{
+                console.log(json);
+                localStorage.setItem('fname_p', json.fname_p);
+                localStorage.setItem('lname_p', json.lname_p);
+                localStorage.setItem('address', json.add_p);
+                localStorage.setItem('birthday', json.bday_p);
+                localStorage.setItem('bio_p', json.bio_p);
+                localStorage.setItem('gender_p', json.gender_p);
+                localStorage.setItem('p_id', json.p_id);
+
+                var p_id = localStorage.getItem("p_id");
+                
+
+                if (acc_type == 1){
+                            location = "mode.html"
+                        }
+                else if(acc_type == 2) {
+                    location = "t_mode.html"
+                } 
+                
+                // var d = new Date();
+                // var n = d.getSeconds();
+                // alert("im here")
+                // if(n==10 || n == 20 || n == 30 || n==40 || n==50  || n==0 ){
+                //     laert("satisfy")
+                //     if (acc_type == 1){
+                //             location = "mode.html"
+                //             alert("go")
+                //     }
+                //     else if(acc_type == 2) {
+                //             location = "t_mode.html"
+                //     } 
+                // }
+                // else{
+                //     console.log(n)
+                    
+                // }
+        
+                console.table(json);
+            }
+            
+        } 
+        else if(acc_type == 2) {
+            location = "t_mode.html"
         }
+
+        
     }
     xhr.send(null);
     // var url  = "https://mighty-badlands-16603.herokuapp.com/api/user/"+acc_id;
@@ -64,27 +98,36 @@ function back(){
 }
 
 function pasuser(form) {
+    
     var uid = form.id.value;
     var pass = form.pass.value;
     
     xhr = new XMLHttpRequest();
-    var url = "https://api-pic-a-talk.herokuapp.com/api/login";
+    var url = "https://cryptic-fjord-60133.herokuapp.com/api/login";
     xhr.open("POST", url, true);
     xhr.setRequestHeader("Authorization", 'Basic ' + btoa(uid + ":" + pass));
     xhr.setRequestHeader("Content-type", "application/json");
     xhr.onreadystatechange = function () { 
         if (xhr.readyState == 4 && xhr.status == 200) {
             var json = JSON.parse(xhr.responseText);
-            alert(json.acc_id);
+
+            if(json.message == "wrong username or password"){
+                alert(json.message)
+            }
+            
+            localStorage.clear();
             localStorage.setItem('acc_id', json.acc_id);
             localStorage.setItem('token', json.token);
-            alert(json.token);
+            localStorage.setItem('acc_type', json.acc_type);
+            localStorage.setItem('email', json.email);
+            console.log(json.token);
+            
             get_info();
-            alert(" Successfully login");
             
 
             console.log(json);
         }
+        
     }
     
     var json =JSON.stringify({"username": uid, "password":pass});
@@ -100,18 +143,40 @@ function pasuser(form) {
 function register_user(form,acc_type){
        
     xhr = new XMLHttpRequest();
-    var url = "https://api-pic-a-talk.herokuapp.com/api/signup";
+    var url = "https://cryptic-fjord-60133.herokuapp.com/api/signup";
     xhr.open("POST", url, true);
     // xhr.setRequestHeader("Authorization", 'Basic ' + btoa(form.id.value + ":" + form.pass.value));
     xhr.setRequestHeader("Content-type", "application/json");
     xhr.onreadystatechange = function () { 
-        alert(xhr.status)
+
         if (xhr.readyState == 4 && xhr.status == 200) {
-            alert("success")
             var json = JSON.parse(xhr.responseText);
-            location="mode.html";  
-            console.log(json.form.id.value +", " + json.form.acc_type.value + ", " + json.form.email.value + ", " + json.form.pass.value);
+            
+            localStorage.clear();
+            localStorage.setItem('acc_id', json.acc_id);
+            localStorage.setItem('acc_type', json.acc_type);
+            localStorage.setItem('token', json.token);
+            localStorage.setItem('email', json.email);
+
+            if(form.acc_type.value == 1){
+                location = "pform.html"
+            }
+            else if(form.acc_type.value == 2){
+                location = "edit_t.html"
+            }
+              
         }
+
+        if (xhr.status == 0){
+            location = "login.html"
+            alert("Sorry the app failed to load.")    
+        }
+
+        if (xhr.status == 500){
+            alert("Sorry but there's some problem in our system. Please come back Later ! ")
+            location = "index.html"
+        }
+
     }
     var json =JSON.stringify({"username": form.id.value,"acc_type":form.acc_type.value, "email":form.email.value, "password":form.pass.value});
     console.log(json)
